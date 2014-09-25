35c3a1690ade09e168f0826c89142403b4f0efa8
FikriFikrulMubarok <fikrifikrulmubarok@y7mail.com>
2014-09-25 14:58:28 +0700

    tugas 2

---
+++ Programsisisegitiga.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.util.Scanner;
+public class Programsisisegitiga{
+	public static void main (String [] args){
+		System.out.println("Program Menghitung Sisi Miring segitiga Siku-siku dengan rumus Phitagoras");
+		int a,t;
+		double sm;
+		Scanner scan=new Scanner(System.in);
+		System.out.print("Masukan Panjang Alas : ");
+		a=scan.nextInt();
+		System.out.print("Masukan Panjang Tinggi : ");
+		t=scan.nextInt();
+		sm = sqrt((a*a)+(t*t));
+		System.out.println();
+		System.out.println("Sisi Segitiga dengan Alas " +a+ " dan Tinggi " +t+ " adalah : " +sm);
+	}
+}
